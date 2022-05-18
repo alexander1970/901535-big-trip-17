ddc80f1505930029526c2cd79508c3f8e0133c8a
alexander1970 <alexander_shloma@mail.ru>
2022-05-18 10:49:32 +0300

    фильтр не работает

--- src/utils/filter .js
+++ src/utils/filter .js
@@ -1,10 +1,9 @@
-import { SortType } from '../mock/consts.js';
+import { SortType } from '../consts.js';
 
 const filter = {
   [SortType.DAY]: (points) => points.filter((point) => point.DAY),
   [SortType.TIME]: (points) => points.filter((point) => point.TIME),
   [SortType.PRICE]: (points) => points.filter((point) => point.PRICE),
-  [SortType.OFFER]: (points) => points.filter((point) => point.OFFER),
 };
 
 export { filter };
